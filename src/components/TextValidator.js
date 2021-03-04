2ec81aa088d5422b67726c605b8ef15683010564
bb2567 <marksong0976@gmail.com>
2021-03-05 01:01:32 +0800

    no message

---
+++ src/components/TextValidator.js
@@ -0,0 +1,33 @@
+import React, { Fragment } from "react";
+import { ValidatorComponent } from "react-form-validator-core";
+
+class TextValidator extends ValidatorComponent {
+    
+  errorText() {
+    const { isValid } = this.state;
+    if (isValid) {
+      return null;
+    }
+    // 錯誤訊息
+    return <span style={{ color: "red" }}>{this.getErrorMessage()}</span>;
+  }
+
+  render() {
+    const {
+      errorMessages,
+      validators,
+      requiredError,
+      validatorListener,
+      ...rest
+    } = this.props;
+
+    return (
+      <Fragment>
+        <input {...rest} />
+        {this.errorText()}
+      </Fragment>
+    );
+  }
+}
+
+export default TextValidator;
