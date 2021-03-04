4586146559e00e77f08d29803bc17d76dfe127dd
bb2567 <marksong0976@gmail.com>
2021-03-04 22:13:30 +0800

    登入頁面

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "../scss/login.scss";
 
-export default function Login() {
+class Login2 extends Component {
+  changeCode(e) {
+    e.target.src = "/api/code/?id=" + Date.now();
+  }
+  render() {
     return (
-        <div>
-            <h1>login</h1>
+      <div className="login-wrap">
+        <nav className="login-nav">
+          <Link className="logo" to="/">
+            <img src="/images/logo.svg" alt="..." />
+          </Link>
+        </nav>
+        <div className="login-bg">
+          <div className="login-box">
+            <div className="input-group">
+              <i class="fas fa-user"></i>
+              <input type="text" id="" placeholder="請輸入電話號碼" />
+            </div>
+            <div className="input-group">
+              <i class="fas fa-lock"></i>
+              <input type="password" id="" placeholder="請輸入密碼" />
+            </div>
+            <div className="code-group input-group">
+              <input
+                type="text"
+                id=""
+                className="code"
+                placeholder="請輸入驗證碼"
+              />
+              <div className="code-img">
+                <img
+                  onClick={(e) => {
+                    this.changeCode(e);
+                  }}
+                  src="/api/code"
+                  alt="..."
+                />
+              </div>
+            </div>
+            <div className="login-btn-group">
+              <button className="login-btn">登入</button>
+              <a href="/" className="link-btn">
+                忘記密碼?
+              </a>
+            </div>
+            <hr />
+            <div className="login-reg">
+              <span>新朋友？</span>
+              <a href="/">註冊</a>
+            </div>
+          </div>
         </div>
-    )
+        <div className="login-ft">&copy;版權所有 SKIGO.com 2016-2022</div>
+      </div>
+    );
+  }
 }
+
+export default Login2;
