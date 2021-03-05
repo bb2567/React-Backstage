7eda3943b4f55ed6a67415e1ce1f93db79ed9e3e
bb2567 <marksong0976@gmail.com>
2021-03-05 12:15:44 +0800

    no message

--- src/components/TextValidator.js
+++ src/components/TextValidator.js
@@ -1,28 +1,33 @@
-import React, { Fragment } from 'react'
-import { ValidatorComponent } from 'react-form-validator-core';
+import React, { Fragment } from "react";
+import { ValidatorComponent } from "react-form-validator-core";
 
 class TextValidator extends ValidatorComponent {
+    
   errorText() {
     const { isValid } = this.state;
     if (isValid) {
-        return null;
+      return null;
     }
-    return (
-      <span style={{ color: 'red',textAlign: 'center', width: '150px', fontSize: '12px',position: 'absolute', left: '100%', top: '5px',padding: '0 0 0 15px' }}>
-          {this.getErrorMessage()}
-      </span>
-    );
+    // 錯誤訊息
+    return <span style={{ color: "red" }}>{this.getErrorMessage()}</span>;
   }
 
-  render () {
-    const { errorMessages, validators, requiredError, validatorListener, ...rest } = this.props;
+  render() {
+    const {
+      errorMessages,
+      validators,
+      requiredError,
+      validatorListener,
+      ...rest
+    } = this.props;
+
     return (
       <Fragment>
         <input {...rest} />
         {this.errorText()}
       </Fragment>
-    )
+    );
   }
 }
 
-export default TextValidator
+export default TextValidator;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { TextValidator } from '../components/';
+import TextValidator from '../components/TextValidator';
 
 import "../scss/login.scss";
 
