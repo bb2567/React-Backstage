e968832ba58fee842f62978e2bddd928dd7a397b
bb2567 <marksong0976@gmail.com>
2021-03-06 00:11:54 +0800

    axios API

---
+++ src/commons/Service.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  userLogin(data) {
+   return axios.post("/api/userlogin", data);
+  },
+};
