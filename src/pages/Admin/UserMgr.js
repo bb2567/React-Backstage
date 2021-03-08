77df4b7550362b0c8ff31dd383807dbc27947a81
bb2567 <marksong0976@gmail.com>
2021-03-08 22:13:55 +0800

    Table 頁面

--- src/pages/Admin/UserMgr.js
+++ src/pages/Admin/UserMgr.js
@@ -1,8 +1,43 @@
 import React, { Component } from "react";
-import { Breadcrumb } from "antd";
+import { Breadcrumb, Table } from "antd";
 import { Link } from "react-router-dom";
 
 class UserMgr extends Component {
+  state = {
+    userData: [
+      {
+        id: 1,
+        name: "Aicoder",
+        phone: "0800000123",
+      },
+      { id: 2, 
+        name: "Aicoder2", 
+        phone: "0800333666" 
+    },
+      {
+        id: 3,
+        name: "Aicoder3",
+        phone: "0800666777",
+      },
+    ],
+    columns: [
+      {
+        key: "id",
+        title: "編號",
+        dataIndex: "id",
+      },
+      {
+        key: "name",
+        title: "名字",
+        dataIndex: "name",
+      },
+      {
+        key: "phone",
+        title: "電話",
+        dataIndex: "phone",
+      }
+    ],
+  };
   render() {
     return (
       <div className="admin-usermgr">
@@ -14,7 +49,11 @@ class UserMgr extends Component {
             <Link to="/home/user_mgr">UserMgr</Link>
           </Breadcrumb.Item>
         </Breadcrumb>
-        <hr/>
+        <hr />
+        <Table style={{backgroundColor:"#fefefe"}}
+          columns={this.state.columns}
+          dataSource={this.state.userData}
+        ></Table>
       </div>
     );
   }
