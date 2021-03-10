94ddd796f3a03884452b1a6a91c4f8e8375d9357
bb2567 <marksong0976@gmail.com>
2021-03-10 13:21:18 +0800

    添加 redux-devtools

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { applyMiddleware, createStore } from "redux";
-import  rootReducer  from "./Reducer/Reducer";
+import rootReducer from "./Reducer/Reducer";
 import thunk from "redux-thunk";
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from "redux-devtools-extension";
 
-
-const store = createStore(rootReducer, {}, applyMiddleware(thunk),);
+const store = createStore(
+  rootReducer,
+  {},
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
- 
